f8e389e92234a3abf99ca281a66ced1a13c4773c
Antoni Sabat <131788519+AntoniSabat@users.noreply.github.com>
2024-05-22 13:54:49 +0200

    repair deployed navigation

--- src/Utils/env.ts
+++ src/Utils/env.ts
@@ -1,9 +1,9 @@
-export const PAGE_PATH = 'sapphirestudio.online';
+export const PAGE_PATH = 'https://www.sapphirestudio.online/';
 export const API_PATH = 'sapphire-b7ndhjlw9-antonisabats-projects.vercel.app';
 export const GRAPHQL_API = "sapphire-b7ndhjlw9-antonisabats-projects.vercel.app/graphql";
 export const CDN_API = 'https://api.imgbb.com/1/upload?key=ced1032cda8964de96c804e90ef55716';
 // todo: zamienić na właściwe ścieki po deployu
 export const KINDE_CLIENT_ID = "be6bc899d09248b8bf1d8c41feddb8ee";
 export const KINDE_DOMAIN = "https://sapphirestudio.kinde.com";
-export const KINDE_REDIRECT_URI = "sapphirestudio.online";
-export const KINDE_LOGOUT_URI = "sapphirestudio.online";
+export const KINDE_REDIRECT_URI = "https://www.sapphirestudio.online/";
+export const KINDE_LOGOUT_URI = "https://www.sapphirestudio.online/";
