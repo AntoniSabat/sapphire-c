4b0360b878fb2e374aa4392526d07599b8a4dac7
Antoni Sabat <131788519+AntoniSabat@users.noreply.github.com>
2024-05-22 13:10:35 +0200

    update client path

--- src/Utils/env.ts
+++ src/Utils/env.ts
@@ -1,9 +1,9 @@
-export const PAGE_PATH = 'http://sapphirestudio.online';
+export const PAGE_PATH = 'https://www.sapphirestudio.online';
 export const API_PATH = 'http://localhost:3000';
 export const GRAPHQL_API = "http://localhost:3000/graphql";
 export const CDN_API = 'https://api.imgbb.com/1/upload?key=ced1032cda8964de96c804e90ef55716';
 // todo: zamienić na właściwe ścieki po deployu
 export const KINDE_CLIENT_ID = "be6bc899d09248b8bf1d8c41feddb8ee";
 export const KINDE_DOMAIN = "https://sapphirestudio.kinde.com";
-export const KINDE_REDIRECT_URI = "http://sapphirestudio.online";
-export const KINDE_LOGOUT_URI = "http://sapphirestudio.online";
+export const KINDE_REDIRECT_URI = "https://www.sapphirestudio.online";
+export const KINDE_LOGOUT_URI = "https://www.sapphirestudio.online";
