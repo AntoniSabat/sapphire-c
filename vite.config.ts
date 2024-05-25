1f46330f2523eeb61b8dfc7d26bcb9194149e6ad
Antoni Sabat <131788519+AntoniSabat@users.noreply.github.com>
2024-05-25 16:31:36 +0200

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+        '/graphql': {
+            target: 'https://test-8sol08mr5-antonisabats-projects.vercel.app/graphql',
+            changeOrigin: true,
+            secure: false,
+        },
+    }
+  },
   plugins: [react()],
 })
