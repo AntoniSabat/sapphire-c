707babe23dc4f89b5d9d2d4170e999903b57ab56
Antoni Sabat <131788519+AntoniSabat@users.noreply.github.com>
2024-05-25 19:20:26 +0200

    Update env.ts

--- src/Utils/env.ts
+++ src/Utils/env.ts
@@ -1,5 +1,5 @@
-// export const PAGE_PATH = 'https://sapphirestudio.online';
-export const PAGE_PATH = 'http://localhost:5173';
+export const PAGE_PATH = 'https://sapphirestudio.online';
+// export const PAGE_PATH = 'http://localhost:5173';
 export const API_PATH = 'https://test-83kl6hmmy-antonisabats-projects.vercel.app';
 // export const API_PATH = 'http://localhost:3000';
 export const GRAPHQL_API = "https://test-83kl6hmmy-antonisabats-projects.vercel.app/graphql";
@@ -8,7 +8,7 @@ export const CDN_API = 'https://api.imgbb.com/1/upload?key=ced1032cda8964de96c80
 // todo: zamienić na właściwe ścieki po deployu
 export const KINDE_CLIENT_ID = "be6bc899d09248b8bf1d8c41feddb8ee";
 export const KINDE_DOMAIN = "https://sapphirestudio.kinde.com";
-export const KINDE_REDIRECT_URI = "http://localhost:5173";
-// export const KINDE_REDIRECT_URI = "https://www.sapphirestudio.online";
-export const KINDE_LOGOUT_URI = "http://localhost:5173";
-// export const KINDE_LOGOUT_URI = "https://www.sapphirestudio.online";
+// export const KINDE_REDIRECT_URI = "http://localhost:5173";
+export const KINDE_REDIRECT_URI = "https://www.sapphirestudio.online";
+// export const KINDE_LOGOUT_URI = "http://localhost:5173";
+export const KINDE_LOGOUT_URI = "https://www.sapphirestudio.online";
